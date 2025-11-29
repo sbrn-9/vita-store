81dfd1419a23cd622835b9ffc603c5badc8e86f0
Sabrina Arevalo <xsabrinarvlx@gmail.com>
2025-11-29 13:14:24 -0300

    Add: navbar

--- vite-web/src/app/page.tsx
+++ vite-web/src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { Text } from "@mantine/core";
+import { Navbar} from "../components/navbar";
+
 
 export default function Home() {
   return (
     <>
-    <Text>Vita Store </Text>     
+      <Navbar />
+      
     </>
   );
 }
--- vite-web/src/components/navbar.tsx
+++ vite-web/src/components/navbar.tsx
@@ -1,9 +1,54 @@
+"use client"; // <-- añade esto en la primera línea del archivo
 
+import { Autocomplete, Burger, Group , Text} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+// import { MantineLogo } from '@mantinex/mantine-logo';
+import React from 'react';
+import styles from './css/navbar.module.css';
+
+const links = [
+  { link: '/about', label: 'Producto' },
+  { link: '/pricing', label: 'Contacto' },
+  { link: '/learn', label: 'Sobre nosotros' },
+  { link: '/community', label: 'Mi carrito' },
+];
 
 export function Navbar() {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={styles.link}
+      onClick={(event) => event.preventDefault()}
+    >
+      {link.label}
+    </a>
+  ));
+
   return (
-    <nav>
-      <h1>Vita Store</h1>
-    </nav>
+    <header className={styles.header}>
+      <div className= {styles.inner}>
+        <Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          <Text>Vita</Text>
+          {/* <MantineLogo size={28} /> */}
+        </Group>
+
+        <Group>
+          <Group ml={50} gap={5} className={styles.links} visibleFrom="sm">
+            {items}
+          </Group>
+          <Autocomplete
+            className={styles.search}
+            placeholder="Search"
+            leftSection="icon"
+            data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
+            visibleFrom="xs"
+          />
+        </Group>
+      </div>
+    </header>
   );
 }
