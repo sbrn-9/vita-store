aed50f75eacdbd9354cd422789cb3a9e3a1690b9
Sabrina Arevalo <xsabrinarvlx@gmail.com>
2025-12-01 12:23:14 -0300

    Add: navbar en el header

--- vite-web/src/components/header.tsx
+++ vite-web/src/components/header.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import React from 'react';
+import styles from './css/header.module.css';
+import { Text, Group } from '@mantine/core';
+import { Navbar } from './navbar';
+
 
 export function Header() {
   return (
     <header>
-      <h1>Vita Store</h1>
+      <Navbar />
+
+      <Group className= {styles.container}>
+        
+        <Text className={styles.subtitle}>EN VITA NOS CENTRAMOS EN TRAER LO MEJOR PARA TU INTERIOR</Text>
+      </Group>
+      
     </header>
   );
 }
